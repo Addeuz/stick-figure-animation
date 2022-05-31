9ac260bf48be2fbef8f9f989cc0202613fafd1b5
Andreas Johansson <andreas.nb.johansson@gmail.com>
2022-05-31 21:19:58 +0200

    First commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let canvas: HTMLCanvasElement;
+
+	onMount(() => {
+		console.log(canvas);
+	});
+</script>
+
+<div>
+	<canvas id="main-canvas" bind:this={canvas} />
+</div>
